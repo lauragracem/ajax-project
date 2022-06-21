c0df67c665b4fc36cf1921e25873b0f9dc6f0ebc
Laura Grace Manimtim <lauragracem@gmail.com>
2022-06-21 22:14:33 +0000

    Visibly render list of habits

--- js/data.js
+++ js/data.js
@@ -1,19 +1,1 @@
 /* exported data */
-var data = {
-  view: 'habit-form',
-  habits: [],
-  editing: null,
-  nextEntryId: 1
-};
-
-var entryJSON = localStorage.getItem('enter-habit');
-if (entryJSON !== null) {
-  data = JSON.parse(entryJSON);
-}
-
-window.addEventListener('beforeunload', entryBase);
-
-function entryBase(event) {
-  var dataJSON = JSON.stringify(data);
-  localStorage.setItem('add-habit', dataJSON);
-}
--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+var data = {
+  view: 'habit-form',
+  graphs: [],
+  editing: null,
+  nextId: '0'
+};
+
 var $loginForm = document.querySelector('#login-form');
 var $userLogin = document.querySelector('.user-login');
 var $habitForm = document.querySelector('.habit-form');
@@ -12,11 +19,11 @@ $loginForm.addEventListener('submit', login);
 
 function login(event) {
   event.preventDefault();
-  getGraphs();
 }
 
 $loginButton.addEventListener('click', nextPage);
 $guestLogin.addEventListener('click', nextPage);
+
 function nextPage(event) {
   switchView('habit-form');
   getGraphs();
@@ -41,27 +48,20 @@ function createHabit(event) {
   $habit.textContent = $enterHabit.value;
   $ul.appendChild($habit);
   createGraph();
+  renderGraphs(data.graphs);
 }
 
 var username = 'ajax-project';
 var secret = 'thisissecret';
-var id = '0';
-
-// get all user's graphs
-// have id start at graphs.length
-
-// on login hit graphs API & get total number of graphs.
-//
 
 function createGraph() {
   var json = {
-    id: 'graph-' + id,
+    id: 'graph-' + data.nextId,
     name: $enterHabit.value,
     unit: 'commit',
     type: 'int',
     color: 'ichou'
   };
-  id++;
 
   var xhr = new XMLHttpRequest();
   xhr.open('POST', 'https://pixe.la/v1/users/' + username + '/graphs');
@@ -69,27 +69,36 @@ function createGraph() {
 
   xhr.responseType = 'json';
   xhr.addEventListener('load', function () {
+    data.nextId++;
+    data.graphs.push(json);
+    renderGraphs(data.graphs);
+    $enterHabit.value = '';
   });
   xhr.send(JSON.stringify(json));
 }
 
 function getGraphs() {
-
   var xhr = new XMLHttpRequest();
   xhr.open('GET', 'https://pixe.la/v1/users/' + username + '/graphs');
   xhr.setRequestHeader('X-USER-TOKEN', secret);
 
   xhr.responseType = 'json';
   xhr.addEventListener('load', function () {
-    // console.log({ xhr });
-    var graphs = xhr.response.graphs;
-    // get graphs from respnose
-    id = graphs.length;
-    // set id to graphs.length
-    // console.log(graphs);
-    // console.log(id);
+    data.graphs = xhr.response.graphs;
+    data.nextId = xhr.response.graphs.length;
+    renderGraphs(data.graphs);
   });
   xhr.send();
+}
+
+function renderGraphs(graphs) {
+  $ul.innerHTML = '';
+  for (var i = 0; i < graphs.length; i++) {
+    var name = graphs[i].name;
+    var $li = document.createElement('li');
+    $li.textContent = name;
+    $ul.appendChild($li);
+  }
 }
 
 // var $tempEl = document.querySelector('.temp');
